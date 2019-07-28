c74d8da97c1b1b21008a711633cbd2137860649d
Martin Peak <martinpeak@gmail.com>
2019-07-28 23:09:28 +1200

    setup staging operator and dag

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -5,6 +5,32 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
+from airflow.contrib.hooks.aws_hook import AwsHook
+import configparser
+
+
+# CONFIG
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
+
+KEY = config.get("AWS", "KEY")
+SECRET = config.get("AWS", "SECRET")
+DWH_CLUSTER_TYPE = config.get("DWH", "DWH_CLUSTER_TYPE")
+DWH_NUM_NODES = config.get("DWH", "DWH_NUM_NODES")
+DWH_NODE_TYPE = config.get("DWH", "DWH_NODE_TYPE")
+DWH_CLUSTER_IDENTIFIER = config.get("DWH", "DWH_CLUSTER_IDENTIFIER")
+DWH_DB = config.get("DWH", "DWH_DB")
+DWH_DB_USER = config.get("DWH", "DWH_DB_USER")
+DWH_DB_PASSWORD = config.get("DWH", "DWH_DB_PASSWORD")
+DWH_PORT = config.get("DWH", "DWH_PORT")
+DWH_IAM_ROLE_NAME = config.get("DWH", "DWH_IAM_ROLE_NAME")
+DWH_ENDPOINT = config.get("DWH", "DWH_ENDPOINT")
+DWH_ROLE_ARN = config.get("DWH", "DWH_ROLE_ARN")
+
+LOG_DATA = config.get("S3", "LOG_DATA")
+LOG_JSONPATH = config.get("S3", "LOG_JSONPATH")
+SONG_DATA = config.get("S3", "SONG_DATA")
+SONG_JSONPATH = config.get("S3", "SONG_JSONPATH")
 
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
@@ -29,12 +55,24 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    iam_role=DWH_ROLE_ARN,
+    s3_bucket=LOG_DATA,
+    aws_credentials_id="aws_credentials",
+    table='staging_events',
+    json_format=LOG_JSONPATH,
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    iam_role=DWH_ROLE_ARN,
+    s3_bucket=SONG_DATA,
+    aws_credentials_id="aws_credentials",
+    table='staging_songs',
+    json_format=SONG_JSONPATH,
 )
 
 load_songplays_table = LoadFactOperator(
--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,26 +1,50 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    
+    copy_sql = """
+    COPY {}
+    FROM {}
+    credentials 'aws_iam_role={}'
+    region 'us-west-2'
+    FORMAT AS JSON {}
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 iam_role="",
+                 json_format="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.s3_bucket = s3_bucket
+        self.iam_role = iam_role
+        self.aws_credentials_id = aws_credentials_id
+        self.json_format = json_format
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        # aws_hook = AwsHook(self.aws_credentials_id)
+        # credentials = aws_hook.get_credentials()
+        redshift_hook = PostgresHook(self.redshift_conn_id)
 
+        self.log.info("Clearing data from destination Redshift table")
+        redshift_hook.run("DELETE FROM {}".format(self.table))
 
-
-
-
+        self.log.info("Copying data from S3 to Redshift")
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+            self.table,
+            self.s3_bucket,
+            self.iam_role,
+            self.json_format
+        )
+        redshift_hook.run(formatted_sql)
